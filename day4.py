30c6e74d6346e2bce69c1e6af3ff2c8fc5a92b5a
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2024-07-22 15:03:45 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -10,10 +10,36 @@ else:
 
 
 # get random value from the list 
-
 import names 
 import random
 
-random_integer = random.randint(0, len(names.names))
+random_integer = random.randint(0, len(names.names) - 1)
 
 print(f"{names.names[random_integer]} is going to by the meal today")
+
+
+
+# Hidden Treasure
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+pos = 0
+ind = int(position[1:2]) - 1
+
+if position[0:1]=="A":
+  pos = 0
+elif position[0:1]=="B":
+  pos = 1
+else:
+  pos = 2
+
+print(pos, ind)
+map[pos][ind] = "X"
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
